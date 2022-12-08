6eb1c1454a97e5467b228d0bd655f7d4d5ee09ee
tera2839 <ten283907@gmail.com>
2022-12-09 03:17:53 +0900

    mod

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfPvhPSf68Ie9zKVIrb5c-47aU_4ha-Z8",
-  authDomain: "twitter-app2-bb475.firebaseapp.com",
-  projectId: "twitter-app2-bb475",
-  storageBucket: "twitter-app2-bb475.appspot.com",
-  messagingSenderId: "160525634190",
-  appId: "1:160525634190:web:fb3fa1e3adc350e5c957df"
+  apiKey: "AIzaSyBnKviSxoegO8k2ppq1D1tcFi2GXjPd6go",
+  authDomain: "twitter-53386.firebaseapp.com",
+  projectId: "twitter-53386",
+  storageBucket: "twitter-53386.appspot.com",
+  messagingSenderId: "949156083262",
+  appId: "1:949156083262:web:968594b855ad5e590e436f"
 };
 
 const app = initializeApp(firebaseConfig);
