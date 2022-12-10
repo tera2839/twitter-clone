41a4f3d7c6ba2c1ec05e21874cebcb4a9748e516
tera2839 <ten283907@gmail.com>
2022-12-11 02:00:04 +0900

    responsive

--- src/components/sidebar/SidebarOption.js
+++ src/components/sidebar/SidebarOption.js
@@ -4,7 +4,16 @@ import "./SidebarOption.css"
 function SidebarOption({text, Icon, active}) {
   return (
     <div className={`sidebarOption ${active && "sidebarOption--active"}`}>
-     <Icon />
+     <Icon sx={{
+      "@media screen and (max-width:1300px)": {
+        paddingBottom: 3,
+        fontSize: 35,
+        marginLeft: 6,
+      }, "@media screen and (max-width:440px)": {
+        marginLeft: 0,
+        
+      },
+     }} />
      <h2>{text}</h2>
     </div>
   )
